4ebbe7ad7acb8fbf48f90510a7fe6b426f7e291c
Pedro H. Ribeiro <133699282+P3dr0HDev@users.noreply.github.com>
2026-08-05 19:21:33 -0300

    Add comment for testing pull functionality
    Added a comment for testing pull functionality.

--- src/main/java/br/com/senac/hello_world/HelloWorldApplication.java
+++ src/main/java/br/com/senac/hello_world/HelloWorldApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class HelloWorldApplication {
 
+	// Testar Pull
+
 	public static void main(String[] args) {SpringApplication.run(HelloWorldApplication.class, args);
 
 		//eu juro que é meu primeiro programa Java!
